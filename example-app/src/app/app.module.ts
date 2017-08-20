b3afaeeee3d5ee413b05b870b2a202d7afe3b2f6
Jonathan Ziller <jonathan.ziller@gmail.com>
2017-08-20 19:04:17 +0200

    chore: add devtools support for example app

--- example-app/src/app/app.module.ts
+++ example-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { HttpModule } from '@angular/http';
 import { MaterialModule, MdNativeDateModule } from '@angular/material';
@@ -26,6 +27,7 @@ import { NgrxMdSelectValueAccessor } from './md-select-value-accessor';
     NgrxFormsModule,
     HttpModule,
     StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({ maxAge: 30 }),
     EffectsModule.forRoot([]),
   ],
   providers: [],
