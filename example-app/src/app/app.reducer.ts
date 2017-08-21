484e6bb76ab1aa2fb821d6e5ed74469fb06b9aa8
Jonathan Ziller <jonathan.ziller@gmail.com>
2017-08-22 00:11:59 +0200

    feat: update example-app to latest version of library

--- example-app/src/app/app.reducer.ts
+++ example-app/src/app/app.reducer.ts
@@ -1,24 +1,22 @@
 import { ActionReducer } from '@ngrx/store';
-import { FormGroupState, createFormGroupReducer, createFormGroupState, SetErrorsAction } from '@ngrx/forms';
+import { FormGroupState, formGroupReducer, createFormGroupState, SetErrorsAction } from '@ngrx/forms';
 
 import { AppState, initialState, ITEM_FORM_ID } from './app.state';
 import { Actions, AddTodoItemAction } from './app.actions';
 import { ItemFormValue, textValidator, priorityValidator, duedateValidator, initialItemFormValue } from './item-form/item-form.state';
 
-const formReducer = createFormGroupReducer(ITEM_FORM_ID, initialItemFormValue);
-
 function validateItemForm(state: FormGroupState<ItemFormValue>) {
   const meta = () => state.controls.meta as FormGroupState<any>;
-  state = formReducer(state, new SetErrorsAction(state.controls.text.id, textValidator(state.value.text)));
-  state = formReducer(state, new SetErrorsAction(meta().controls.priority.id, priorityValidator(state.value.meta.priority)));
-  state = formReducer(state, new SetErrorsAction(meta().controls.duedate.id, duedateValidator(state.value.meta.duedate)));
+  state = formGroupReducer(state, new SetErrorsAction(state.controls.text.id, textValidator(state.value.text)));
+  state = formGroupReducer(state, new SetErrorsAction(meta().controls.priority.id, priorityValidator(state.value.meta.priority)));
+  state = formGroupReducer(state, new SetErrorsAction(meta().controls.duedate.id, duedateValidator(state.value.meta.duedate)));
   return state;
 }
 
 const initialItemFormState = validateItemForm(initialState.itemForm);
 
 export function appReducer(state = { ...initialState, itemForm: initialItemFormState }, action: Actions): AppState {
-  const itemForm = formReducer(state.itemForm, action);
+  const itemForm = formGroupReducer(state.itemForm, action);
   if (itemForm !== state.itemForm) {
     state = { ...state, itemForm: validateItemForm(itemForm) };
   }
