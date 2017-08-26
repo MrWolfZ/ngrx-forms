5b790dfaa7a83257b75de1293bc8e1719674dd5a
Jonathan Ziller <jonathan.ziller@gmail.com>
2017-08-26 22:36:59 +0200

    feat: add update functions for all actions

--- src/update-functions.spec.ts
+++ src/update-functions.spec.ts
@@ -1,9 +1,23 @@
 import { SetValueAction, MarkAsTouchedAction } from './actions';
-import { FormGroupState, createFormGroupState } from './state';
+import { FormGroupState, createFormGroupState, cast } from './state';
 import {
   updateGroup,
   groupUpdateReducer,
+  setValue,
   validate,
+  enable,
+  disable,
+  markAsDirty,
+  markAsPristine,
+  markAsTouched,
+  markAsUntouched,
+  markAsSubmitted,
+  markAsUnsubmitted,
+  focus,
+  unfocus,
+  setLastKeyDownCode,
+  addControl,
+  removeControl,
 } from './update-functions';
 
 describe('update functions', () => {
@@ -11,7 +25,7 @@ describe('update functions', () => {
   const FORM_CONTROL_INNER_ID = FORM_CONTROL_ID + '.inner';
   interface NestedValue { inner4: string; }
   interface FormGroupValue { inner: string; inner2?: string; inner3?: NestedValue; }
-  const INITIAL_FORM_CONTROL_VALUE: FormGroupValue = { inner: '', inner2: '', inner3: { inner4: '' } };
+  const INITIAL_FORM_CONTROL_VALUE: FormGroupValue = { inner: '', inner3: { inner4: '' } };
   const INITIAL_STATE = createFormGroupState(FORM_CONTROL_ID, INITIAL_FORM_CONTROL_VALUE);
 
   describe(updateGroup.name, () => {
@@ -81,17 +95,169 @@ describe('update functions', () => {
     });
   });
 
-  describe(validate.name, () => {
-    it('should set errors for controls', () => {
-      const errors = { required: true };
-      const resultState = validate<string>(() => errors)(INITIAL_STATE.controls.inner);
-      expect(resultState.errors).toBe(errors);
+  describe(setValue.name, () => {
+    it('should call reducer for controls', () => {
+      const resultState = setValue<string>('A')(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
     });
 
-    it('should set errors for groups', () => {
+    it('should call reducer for groups', () => {
+      const resultState = setValue<FormGroupValue>({ inner: 'A' })(INITIAL_STATE);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(validate.name, () => {
+    it('should call reducer for controls', () => {
+      const errors = { required: true };
+      const resultState = validate<string>(() => errors)(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
       const errors = { required: true };
       const resultState = validate<FormGroupValue>(() => errors)(INITIAL_STATE);
-      expect(resultState.errors).toEqual(errors);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(enable.name, () => {
+    it('should call reducer for controls', () => {
+      const state = { ...INITIAL_STATE.controls.inner, isEnabled: false, isDisabled: true };
+      const resultState = enable(cast(state));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const state = { ...INITIAL_STATE, isEnabled: false, isDisabled: true };
+      const resultState = enable(state);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(disable.name, () => {
+    it('should call reducer for controls', () => {
+      const resultState = disable(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const resultState = disable(INITIAL_STATE);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(markAsDirty.name, () => {
+    it('should call reducer for controls', () => {
+      const resultState = markAsDirty(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const resultState = markAsDirty(INITIAL_STATE);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(markAsPristine.name, () => {
+    it('should call reducer for controls', () => {
+      const state = { ...INITIAL_STATE.controls.inner, isDirty: false, isPristine: true };
+      const resultState = markAsPristine(cast(state));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const state = { ...INITIAL_STATE, isDirty: false, isPristine: true };
+      const resultState = markAsPristine(state);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(markAsTouched.name, () => {
+    it('should call reducer for controls', () => {
+      const resultState = markAsTouched(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const resultState = markAsTouched(INITIAL_STATE);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(markAsUntouched.name, () => {
+    it('should call reducer for controls', () => {
+      const state = { ...INITIAL_STATE.controls.inner, isTouched: false, isUntouched: true };
+      const resultState = markAsUntouched(cast(state));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const state = { ...INITIAL_STATE, isTouched: false, isUntouched: true };
+      const resultState = markAsUntouched(state);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(markAsSubmitted.name, () => {
+    it('should call reducer for controls', () => {
+      const resultState = markAsSubmitted(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const resultState = markAsSubmitted(INITIAL_STATE);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(markAsUnsubmitted.name, () => {
+    it('should call reducer for controls', () => {
+      const state = { ...INITIAL_STATE.controls.inner, isSubmitted: false, isUnsubmitted: true };
+      const resultState = markAsUnsubmitted(cast(state));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+
+    it('should call reducer for groups', () => {
+      const state = { ...INITIAL_STATE, isSubmitted: false, isUnsubmitted: true };
+      const resultState = markAsUnsubmitted(state);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(focus.name, () => {
+    it('should call reducer for controls', () => {
+      const resultState = focus(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+  });
+
+  describe(unfocus.name, () => {
+    it('should call reducer for controls', () => {
+      const state = { ...INITIAL_STATE.controls.inner, isSubmitted: false, isUnsubmitted: true };
+      const resultState = unfocus(cast(state));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+  });
+
+  describe(setLastKeyDownCode.name, () => {
+    it('should call reducer for controls', () => {
+      const resultState = setLastKeyDownCode(12)(cast(INITIAL_STATE.controls.inner));
+      expect(resultState).not.toBe(cast(INITIAL_STATE.controls.inner));
+    });
+  });
+
+  describe(addControl.name, () => {
+    it('should call reducer for groups', () => {
+      const resultState = addControl<FormGroupValue, 'inner2'>('inner2', 'A')(INITIAL_STATE);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
+    });
+  });
+
+  describe(removeControl.name, () => {
+    it('should call reducer for groups', () => {
+      const resultState = removeControl<FormGroupValue>('inner3')(INITIAL_STATE);
+      expect(resultState).not.toBe(cast(INITIAL_STATE));
     });
   });
 });
--- src/update-functions.ts
+++ src/update-functions.ts
@@ -1,7 +1,31 @@
 import { Action } from '@ngrx/store';
 
-import { SetErrorsAction } from './actions';
-import { AbstractControlState, FormGroupState, FormGroupControls, ValidationErrors } from './state';
+import {
+  SetValueAction,
+  SetErrorsAction,
+  EnableAction,
+  DisableAction,
+  MarkAsDirtyAction,
+  MarkAsPristineAction,
+  MarkAsTouchedAction,
+  MarkAsUntouchedAction,
+  MarkAsSubmittedAction,
+  MarkAsUnsubmittedAction,
+  FocusAction,
+  UnfocusAction,
+  SetLastKeyDownCodeAction,
+  AddControlAction,
+  RemoveControlAction
+} from './actions';
+import {
+  AbstractControlState,
+  FormControlState,
+  FormGroupState,
+  FormGroupControls,
+  KeyValue,
+  ValidationErrors,
+  FormControlValueTypes,
+} from './state';
 import { formControlReducer } from './control/reducer';
 import { formGroupReducer } from './group/reducer';
 import { isGroupState, computeGroupState } from './group/reducer/util';
@@ -49,6 +73,85 @@ function abstractControlReducer<TValue>(state: AbstractControlState<TValue>, act
   return isGroupState(state) ? formGroupReducer(state as any, action) as any : formControlReducer(state as any, action);
 }
 
+export function setValue<TValue extends FormControlValueTypes>(value: TValue): ProjectFn<FormControlState<TValue>>;
+export function setValue<TValue extends KeyValue>(value: TValue): ProjectFn<FormGroupState<TValue>>;
+export function setValue<TValue>(value: TValue) {
+  return (state: AbstractControlState<TValue>) => abstractControlReducer(state, new SetValueAction(state.id, value));
+}
+
+export function validate<TValue extends FormControlValueTypes>(validatorFn: (value: TValue) => ValidationErrors): ProjectFn<FormControlState<TValue>>;
+export function validate<TValue extends KeyValue>(validatorFn: (value: TValue) => ValidationErrors): ProjectFn<FormGroupState<TValue>>;
 export function validate<TValue>(validatorFn: (value: TValue) => ValidationErrors) {
   return (state: AbstractControlState<TValue>) => abstractControlReducer(state, new SetErrorsAction(state.id, validatorFn(state.value)));
+}
+
+export function enable<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function enable<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function enable<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new EnableAction(state.id));
+}
+
+export function disable<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function disable<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function disable<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new DisableAction(state.id));
+}
+
+export function markAsDirty<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function markAsDirty<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function markAsDirty<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new MarkAsDirtyAction(state.id));
+}
+
+export function markAsPristine<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function markAsPristine<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function markAsPristine<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new MarkAsPristineAction(state.id));
+}
+
+export function markAsTouched<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function markAsTouched<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function markAsTouched<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new MarkAsTouchedAction(state.id));
+}
+
+export function markAsUntouched<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function markAsUntouched<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function markAsUntouched<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new MarkAsUntouchedAction(state.id));
+}
+
+export function markAsSubmitted<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function markAsSubmitted<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function markAsSubmitted<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new MarkAsSubmittedAction(state.id));
+}
+
+export function markAsUnsubmitted<TValue extends FormControlValueTypes>(state: FormControlState<TValue>): FormControlState<TValue>;
+export function markAsUnsubmitted<TValue extends KeyValue>(state: FormGroupState<TValue>): FormGroupState<TValue>;
+export function markAsUnsubmitted<TValue>(state: AbstractControlState<TValue>) {
+  return abstractControlReducer(state, new MarkAsUnsubmittedAction(state.id));
+}
+
+export function focus<TValue extends FormControlValueTypes>(state: FormControlState<TValue>) {
+  return formControlReducer(state, new FocusAction(state.id));
+}
+
+export function unfocus<TValue extends FormControlValueTypes>(state: FormControlState<TValue>) {
+  return formControlReducer(state, new UnfocusAction(state.id));
+}
+
+export function setLastKeyDownCode<TValue extends FormControlValueTypes>(code: number) {
+  return (state: FormControlState<TValue>) => formControlReducer(state, new SetLastKeyDownCodeAction(state.id, code));
+}
+
+export function addControl<TValue extends KeyValue, TControlKey extends keyof TValue>(
+  name: keyof TValue,
+  value: TValue[TControlKey],
+) {
+  return (state: FormGroupState<TValue>) => formGroupReducer(state, new AddControlAction<TValue, TControlKey>(state.id, name, value));
+}
+
+export function removeControl<TValue extends KeyValue>(name: keyof TValue) {
+  return (state: FormGroupState<TValue>) => formGroupReducer(state, new RemoveControlAction<TValue>(state.id, name));
 }
