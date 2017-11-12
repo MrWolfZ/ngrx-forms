4837444b5b555403806144163dc9e5a5ff4a4c7f
Jonathan Ziller <jonathan.ziller@gmail.com>
2017-11-12 21:40:25 +0100

    refactor: some minor cleanup

--- src/value-accessors/radio.ts
+++ src/value-accessors/radio.ts
@@ -1,18 +1,12 @@
-import { Directive, ElementRef, forwardRef, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, forwardRef, HostListener, Input, Renderer2 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { FormControlState } from '../state';
 
-// tslint:disable:directive-selector
-// tslint:disable:use-host-property-decorator
 // tslint:disable:directive-class-suffix
 
 @Directive({
   selector: 'input[type=radio][ngrxFormControlState]',
-  host: {
-    '(change)': 'onChange()',
-    '(blur)': 'onTouched()',
-  },
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => NgrxRadioControlValueAccessor),
@@ -35,8 +29,12 @@ export class NgrxRadioControlValueAccessor implements ControlValueAccessor {
 
   private latestValue: any;
   private isChecked: boolean;
-  private onChange: () => void = () => void 0;
-  private onTouched: () => void = () => void 0;
+
+  @HostListener('change')
+  onChange: () => void = () => void 0
+
+  @HostListener('blur')
+  onTouched: () => void = () => void 0
 
   constructor(
     private renderer: Renderer2,
