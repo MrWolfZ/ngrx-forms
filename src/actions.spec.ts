4e169a6960bd4913b784ff228fe4980fd477744b
Luca Trazzi <lucax88x@gmail.com>
2018-02-07 15:51:19 +0100

    testing this simple function

---
+++ src/actions.spec.ts
@@ -0,0 +1,16 @@
+import { isNgrxFormsAction } from './actions';
+
+describe('action', () => {
+  it('should return true if type is ngrx/forms/', () => {
+    expect(isNgrxFormsAction({ type: 'ngrx/forms/' })).toBeTruthy();
+  });
+  it('should return true if type startsWith ngrx/forms/', () => {
+    expect(isNgrxFormsAction({ type: 'ngrx/forms/test' })).toBeTruthy();
+  });
+  it('should return false if type does not have ngrx/forms/', () => {
+    expect(isNgrxFormsAction({ type: 'some-type' })).toBeFalsy();
+  });
+  it('should return false if type is missing', () => {
+    expect(isNgrxFormsAction({} as any)).toBeFalsy();
+  });
+});
