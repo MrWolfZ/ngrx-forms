fdb2e22d0ff753492f610db904355b60a32e5c04
Luca Trazzi <lucax88x@gmail.com>
2018-02-12 16:19:19 +0100

    line between each test

--- src/actions.spec.ts
+++ src/actions.spec.ts
@@ -4,12 +4,15 @@ describe('action', () => {
   it('should return true if type is ngrx/forms/', () => {
     expect(isNgrxFormsAction({ type: 'ngrx/forms/' })).toBeTruthy();
   });
+
   it('should return true if type startsWith ngrx/forms/', () => {
     expect(isNgrxFormsAction({ type: 'ngrx/forms/test' })).toBeTruthy();
   });
+
   it('should return false if type does not have ngrx/forms/', () => {
     expect(isNgrxFormsAction({ type: 'some-type' })).toBeFalsy();
   });
+
   it('should return false if type is missing', () => {
     expect(isNgrxFormsAction({} as any)).toBeFalsy();
   });
