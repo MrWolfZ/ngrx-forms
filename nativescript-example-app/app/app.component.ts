4bbce117794a829fac5277bf81c3f5e007258c16
Jonathan Ziller <jonathan.ziller@gmail.com>
2018-04-07 22:56:43 +0200

    feat(nativescript example app): add simple form view adapter for text fields

--- nativescript-example-app/app/app.component.ts
+++ nativescript-example-app/app/app.component.ts
@@ -1,15 +1,86 @@
 import { Component } from '@angular/core';
-import { createFormControlState } from 'ngrx-forms';
+import { createFormControlState, FormControlState } from 'ngrx-forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+// workaround for debugging app while not being connected to a debugger
+const logs: string[] = [];
+console.log = (message, ...args) => {
+  logs.push(message);
+  args.forEach(a => logs.push(a));
+};
+
+console.error = (message, ...args) => {
+  logs.push(message);
+  args.forEach(a => logs.push(a));
+};
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'my-app',
   template: `
     <ActionBar title="My App" class="action-bar"></ActionBar>
-    <input type="text" [ngrxFormControlState]="state">
+    <StackLayout orientation="vertical">
+      <TextField hint="Input" [ngrxFormControlState]="controlState$ | async"></TextField>
+      <Label [text]="(controlState$ | async).value"></Label>
+      <Label *ngFor="let l of logs" [text]="l"></Label>
+    </StackLayout>
   `,
 })
 export class AppComponent {
-  // Your TypeScript logic goes here
-  state = createFormControlState('test control', 'initial value');
+  controlState$: Observable<FormControlState<string>>;
+  logs = logs;
+
+  constructor(store: Store<any>) {
+    this.controlState$ = store.select(s => s.controlState);
+  }
+}
+
+import { Directive, ElementRef, forwardRef, HostListener, Input, Renderer2 } from '@angular/core';
+
+import { FormViewAdapter, NGRX_FORM_VIEW_ADAPTER } from 'ngrx-forms';
+
+// tslint:disable:directive-class-suffix
+
+@Directive({
+  selector: 'TextField[ngrxFormControlState]',
+  providers: [{
+    provide: NGRX_FORM_VIEW_ADAPTER,
+    useExisting: forwardRef(() => NgrxTextFieldViewAdapter),
+    multi: true,
+  }],
+})
+export class NgrxTextFieldViewAdapter implements FormViewAdapter {
+  onChange: (value: any) => void = () => void 0;
+
+  @HostListener('blur')
+  onTouched: () => void = () => void 0
+
+  @Input() set ngrxFormControlState(value: FormControlState<any>) {
+    if (!value) {
+      throw new Error('The control state must not be undefined!');
+    }
+  }
+
+  setViewValue(value: any): void {
+    // const normalizedValue = value == null ? '' : value;
+    // this.renderer.setProperty(this.elementRef.nativeElement, 'value', normalizedValue);
+  }
+
+  setOnChangeCallback(fn: (value: any) => void): void {
+    this.onChange = fn;
+  }
+
+  setOnTouchedCallback(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setIsDisabled(isDisabled: boolean): void {
+    // this.renderer.setProperty(this.elementRef.nativeElement, 'disabled', isDisabled);
+  }
+
+  @HostListener('textChange', ['$event'])
+  handleInput(event: any): void {
+    this.onChange(event.object.text);
+  }
 }
--- nativescript-example-app/app/app.module.ts
+++ nativescript-example-app/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-import { NgrxFormsModule } from 'ngrx-forms';
+import { NgrxFormsModule, formControlReducer, createFormControlState } from 'ngrx-forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent, NgrxTextFieldViewAdapter } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NgrxTextFieldViewAdapter],
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, StoreModule.forRoot({}), NgrxFormsModule],
+  imports: [NativeScriptModule, CommonModule, StoreModule.forRoot<any>({
+    controlState: formControlReducer,
+  }, {
+    initialState: {
+      controlState: createFormControlState('test control', 'initial value'),
+    },
+  }), NgrxFormsModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
