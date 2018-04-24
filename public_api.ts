87ce11a2141e2de7d8a4ace5da59bcc99e1937f7
Jonathan Ziller <jonathan.ziller@gmail.com>
2018-04-24 20:50:43 +0200

    docu: adjust license comments

--- public_api.ts
+++ public_api.ts
@@ -1,9 +1,8 @@
 /**
- * Ngrx Forms
- * Proper integration of forms in Angular 4 applications using Ngrx
+ * ngrx-forms
+ * Proper integration of forms in Angular applications using Ngrx
  * Written by Jonathan Ziller.
  * MIT license.
  * https://github.com/MrWolfZ/ngrx-forms
  */
-
 export * from './src/ngrx-forms';
--- validation/public_api.ts
+++ validation/public_api.ts
@@ -1,6 +1,6 @@
 /**
- * Ngrx Forms
- * Proper integration of forms in Angular 4 applications using Ngrx
+ * ngrx-forms
+ * Proper integration of forms in Angular applications using Ngrx
  * Written by Jonathan Ziller.
  * MIT license.
  * https://github.com/MrWolfZ/ngrx-forms
