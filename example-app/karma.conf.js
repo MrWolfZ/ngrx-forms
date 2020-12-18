1f760d97be7b5ec96d6d779bb362b65dbf065692
Jonathan Ziller <jonathan.ziller@gmail.com>
2020-12-18 12:17:59 +0100

    chore(example app): update karma dependency

--- example-app/karma.conf.js
+++ example-app/karma.conf.js
@@ -1,32 +1,51 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-spec-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), 
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+
+    reporters: ["spec"],
+
+    specReporter: {
+      maxLogLines: 10,
+      suppressErrorSummary: true,
+      suppressFailed: false,
+      suppressPassed: true,
+      suppressSkipped: true,
+      showSpecTiming: false,
+      failFast: false,
     },
-    
-    reporters: ['progress', 'kjhtml'],
+
+    // web server port
     port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ["ChromeHeadless"],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
   });
 };
