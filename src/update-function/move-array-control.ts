903c15a87fa7e7be383bfd7e3afb6940e94c0230
Jonathan Ziller <jonathan.ziller@gmail.com>
2020-12-18 11:52:12 +0100

    refactor: tweak generics for `moveArrayControl` update function

--- src/update-function/move-array-control.ts
+++ src/update-function/move-array-control.ts
@@ -1,13 +1,13 @@
 import { MoveArrayControlAction } from '../actions';
 import { formArrayReducer } from '../array/reducer';
-import { FormArrayState, isArrayState, KeyValue } from '../state';
+import { FormArrayState, isArrayState } from '../state';
 import { ensureState } from './util';
 
 /**
  * This update function takes a source index, a destination index, and returns a projection function
  * that moves the child control at the source index to the destination index from a form array state.
  */
-export function moveArrayControl<TValue extends KeyValue>(fromIndex: number, toIndex: number): (state: FormArrayState<TValue>) => FormArrayState<TValue>;
+export function moveArrayControl(fromIndex: number, toIndex: number): <TValue>(state: FormArrayState<TValue>) => FormArrayState<TValue>;
 
 /**
  * This update function takes a form array state, a source index, a destination index and moves the
